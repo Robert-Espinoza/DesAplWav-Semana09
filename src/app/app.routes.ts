80c815b63cb199b7a012fab3cffc6306f3797e3d
Robert-Espinoza <robert.espinoza.s@tecsup.edu.pe>
2024-05-09 23:16:46 -0500

    Se agrega el proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { Route } from '@angular/router';
+import { ProveedorComponent } from './proveedor/proveedor.component';
+import { ProductoComponent } from './producto/producto.component';
+import { FacturaComponent } from './factura/factura.component';
+import { ClienteComponent } from './cliente/cliente.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
 
-export const routes: Routes = [];
+export const routes: Route[] = [
+  { path: 'proveedor', component: ProveedorComponent },
+  { path: 'producto', component: ProductoComponent },
+  { path: 'factura', component: FacturaComponent },
+  { path: 'cliente', component: ClienteComponent },
+  { path: 'empleado', component: EmpleadoComponent },
+];
